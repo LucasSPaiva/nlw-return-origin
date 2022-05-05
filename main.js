f3c5d34cc2d18ed262af1eb5de12134b265e2ba6
LucasSPaiva <lukas.souza.paiva.9@gmail.com>
2022-05-05 15:19:43 -0300

    :tada: Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// Resolve o problema de carregamento do js depois da página
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+	showNavigationOnScroll()
+	showArrowOnScroll()
+}
+
+function showNavigationOnScroll() {
+	if (scrollY > 0) {
+		navigation.classList.add('scroll')
+	} else {
+		navigation.classList.remove('scroll')
+	}
+}
+
+function showArrowOnScroll() {
+	if (scrollY > 300) {
+		arrow.classList.add('scroll')
+	} else {
+		arrow.classList.remove('scroll')
+	}
+}
+
+function openMenu() {
+	document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+	document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+	origin: 'top',
+	distance: '30px',
+	duration: 700
+}).reveal(`
+	#hero .button,
+  #hero img, 
+  #hero .details .items, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about img,
+	#contact,
+	#contact .content,
+	#contact .button,
+	footer,
+	footer #copyright,
+	footer #social-links`)
